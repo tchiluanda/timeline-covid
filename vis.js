dac675bf8550c77f2abae2f3f7111a5a77836cc7
tiagombp <tiagombp@gmail.com>
2021-07-01 18:05:21 -0300

    estrutura primeira versão do streamgraph

--- vis.js
+++ vis.js
@@ -1,5 +1,44 @@
 const vis = {
 
+    data : {
+
+        raw : null,
+
+        stacked : null,
+
+        load : () => {
+
+            d3.csv("./gastos.csv", d => {
+
+                d['date'] = d3.timeParse("%Y-%m-%d")(d.mes_lancamento);
+                d["data_br"] = d3.timeFormat("%B de %Y")(d.date);
+
+                return d
+
+            })
+              .then(function(dados) {
+
+                vis.data.raw = dados;
+                vis.ctrl.begin();
+
+
+            })
+
+        },
+
+        make_stack : (data) => {
+
+            const stacked = d3.stack()
+             .keys(data.columns.slice(2))
+             .offset(d3.stackOffsetWiggle)
+             .order(d3.stackOrderInsideOut)(data)
+
+            return stacked;
+
+        }
+
+    },
+
     svg_background : {
 
         ref : "#background",
@@ -65,6 +104,163 @@ const vis = {
 
     },
 
+    stream : {
+
+        refs : {
+
+            svg: 'svg.stream',
+
+            container : 'div.stream-wrapper'
+
+        },
+
+        sizes : {
+
+            width: null,
+
+            height: 1000,
+
+            margins : {
+
+                left: 0,
+                right: 20,
+                bottom: 0,
+                top: 0
+
+            },
+
+            get : function() {
+
+                const cont = document.querySelector(vis.stream.refs.container);
+
+                this.width = +window.getComputedStyle(cont).getPropertyValue("width").slice(0,-2)
+
+            },
+
+            set : function() {
+
+                const svg = document.querySelector(vis.stream.refs.svg);
+
+                svg.style.height = this.height + 'px';
+
+            }
+
+        },
+
+        scales : {
+
+            x : null,
+
+            y : null,
+
+            color : null,
+
+            set : function() {
+
+                // y
+
+                const data = vis.data.raw;
+                const series = vis.data.stacked;
+
+                const margin = vis.stream.sizes.margins;
+
+                const sizes = vis.stream.sizes;
+
+                this.y = d3.scaleTime()
+                  .domain(d3.extent(data, d => d.date))
+                  .range([margin.top, sizes.height - margin.bottom]);
+
+                // x
+
+                this.x = d3.scaleLinear()
+                  .domain(
+                      [
+                          d3.min(series, d => d3.min(d, d => d[0])), 
+                          d3.max(series, d => d3.max(d, d => d[1]))
+                        ])
+                  .range([margin.left, sizes.width - margin.right]);
+
+                // color
+
+                this.color = d3.scaleOrdinal()
+                  .domain(data.columns.slice(2))
+                  .range(d3.schemeCategory10);
+
+            }
+
+        },
+
+        area : {
+            
+            generator : d3.area(),
+
+            set : function() {
+
+                const scales = vis.stream.scales;
+
+                vis.stream.area.generator
+                  .y(d => scales.y(d.data.date))
+                  .x0(d => scales.x(d[0]))
+                  .x1(d => scales.x(d[1]));
+
+            }
+
+        },
+
+
+        draw : function() {
+
+            const svg = d3.select(vis.stream.refs.svg);
+            const series = vis.data.stacked;
+            const area = vis.stream.area.generator;
+            const color = vis.stream.scales.color;
+
+            svg.append("g")
+              .selectAll("path")
+              .data(series)
+              .join("path")
+              .attr("fill", ({key}) => color(key))
+              .attr("d", area)
+              .append("title")
+              .text(({key}) => key);
+        
+            //svg.append("g")
+            //  .call(xAxis);
+
+
+        }
+
+        
+
+    },
+
+    utils :  {
+
+        localeBrasil : {
+
+            "decimal": ",",
+            "thousands": ".",
+            "grouping": [3],
+            "currency": ["R$", ""]
+
+        },
+          
+        //https://cdn.jsdelivr.net/npm/d3-time-format@2/locale/pt-BR.json
+        localeDataBrasil : {
+
+            "dateTime": "%A, %e de %B de %Y. %X",
+            "date": "%d/%m/%Y",
+            "time": "%H:%M:%S",
+            "periods": ["AM", "PM"],
+            "days": ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+            "shortDays": ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+            "months": ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+            "shortMonths": ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
+        
+        }
+
+    },
+
 
 
     ctrl : {
@@ -75,6 +271,27 @@ const vis = {
             vis.svg_background.set_svg_size();
             vis.line.get_left_space();
             vis.line.draw();
+            
+            vis.data.load();
+
+        },
+
+        // after data is loaded
+        begin : function() {
+
+            vis.data.stacked  = vis.data.make_stack(vis.data.raw);
+
+            vis.stream.sizes.get();
+            vis.stream.sizes.set();
+
+
+            vis.stream.scales.set();
+
+            vis.stream.area.set();
+
+            vis.stream.draw();
+
+
         }
 
     }
